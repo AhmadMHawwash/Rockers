9cdd71ab997ca7450fa18aba5111dca9d5958ca1
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2020-01-22 12:00:52 +0200

    persist form state in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
+import { Provider } from "react-redux";
 import "./App.css";
 import Form from "./Components/Form/Form";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./store/rootReducer";
+
+const safeJSONObject = "{}";
+const persistedState = JSON.parse(
+  localStorage.getItem("rockers") || safeJSONObject
+);
 
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState: persistedState
+});
+
+store.subscribe(() => {
+  localStorage.setItem("rockers", JSON.stringify(store.getState()));
 });
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Form />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Form />
+      </div>
     </Provider>
   );
 };
