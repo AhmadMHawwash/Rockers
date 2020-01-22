44dfac472ebbe93368c5f417d3f25057fcf28fc8
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2020-01-22 12:05:51 +0200

    adding comments to utilities

--- src/Components/Form/utilities.ts
+++ src/Components/Form/utilities.ts
@@ -3,10 +3,12 @@ import { defaultCountry } from "./Form";
 //SSN is valid if the number is (10 || 12) digits
 const validSSNPatterns = [/^\d{10}$/, /^\d{12}$/];
 
+//checks for valid SSNs base on validSSNPatterns
 export const isValidSocialSecurityNumber = (SSN: string = "") => {
   return validSSNPatterns.some(pattern => pattern.test(SSN));
 };
 
+//checks for valid Swedish phones
 export const isValidSwedishTelephoneNumber = (telephone: string) => {
   if (!telephone) return false;
 
@@ -18,12 +20,15 @@ export const isValidSwedishTelephoneNumber = (telephone: string) => {
   return isLeadingZero && isValidRestLength;
 };
 
+//checks for valid email addresses
 export const isValidEmailAddress = (email: string = "") => {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(String(email).toLowerCase());
 };
 
+// checks for valid country
 export const isValidCountry = (selectedCountry: string) =>
   selectedCountry !== defaultCountry.name;
 
+//check if given value is number
 export const isNumber = (n: any) => !isNaN(n);
