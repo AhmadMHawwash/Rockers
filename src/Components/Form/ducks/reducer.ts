db02c343967c9996607cf829072d9d71ff40cc20
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2020-01-22 11:58:40 +0200

    createReducer for form state

--- src/Components/Form/ducks/reducer.ts
+++ src/Components/Form/ducks/reducer.ts
@@ -1,11 +1,41 @@
 import { createReducer } from "@reduxjs/toolkit";
-import { simple } from "./actions";
+import { defaultCountry, FormState } from "../Form";
+import { setFormFieldState, submitFormState } from "./actions";
+import { getFormErrors } from "./reducersHelpers";
 
-export default createReducer(
-  {},
-  {
-    [simple.type]: (state, action) => {
-      console.log(state, action);
-    }
+const initialState: FormState = {
+  fields: {
+    country: defaultCountry.name,
+    phone: "",
+    ssn: "",
+    email: ""
+  },
+  hasError: {
+    country: false,
+    phone: false,
+    ssn: false,
+    email: false
   }
-);
+};
+
+export default createReducer(initialState, {
+  [setFormFieldState.type]: (
+    state,
+    action: ReturnType<typeof setFormFieldState>
+  ) => {
+    return {
+      ...state,
+      fields: {
+        ...state.fields,
+        [action.payload.name]: action.payload.value
+      }
+    };
+  },
+  [submitFormState.type]: (state, action) => {
+    const errors: FormState["hasError"] = getFormErrors(state.fields);
+    return {
+      ...state,
+      hasError: errors
+    };
+  }
+});
